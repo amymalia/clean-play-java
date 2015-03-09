99581f36000a9e0d04a16d3493fdec3977d398b5
amymalia <amytakayesu@gmail.com>
2015-03-08 18:47:38 -1000

    Cleaned code
    Used check style and optimized imports and reformatted

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
-import views.html.*;
-
+/**
+ * Adds controllers to application.
+ */
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  /**
+   * Application controller for the home page.
+   * @return rendered page
+   */
+  public static Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
 
 }
---
+++ test/tests/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by amytakayesu on 3/8/15.
+ */
+package tests;
